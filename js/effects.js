75f08fe60ece6b31f4d0784b8a3f1f1bc834b56d
Jørgen Jacobsen <retrojorgen@Jrgens-MacBook-Air.local>
2014-01-16 21:07:53 +0100

    styled with category to the left

---
+++ js/effects.js
@@ -0,0 +1,28 @@
+var showSubSection = function(event) {
+      var category = $(event.target).closest('.category').attr('data-category');
+      var categorySubsection = $(event.target).closest('.category-subsection').attr('data-subcategory');
+      $('.category-subsection').addClass('visible');
+      if(category !== undefined) {
+        getSubSection(category, function(category) {
+
+        });
+      } else {
+        $('.category[data-category=' + categorySubsection + ']').addClass('active');
+      }
+    },
+    hideSubSection = function(event) {
+      $('.category-subsection').removeClass('visible');
+      $('.category').removeClass('active');
+    }
+    getSubSection = function(category, callback) {
+      $('.category-subsection').attr('data-subcategory', category);
+      
+      $
+    };
+
+//listeners
+$('.category').on('mouseover', showSubSection);
+$('.category').on('mouseout', hideSubSection);
+
+$('.category-subsection').on('mouseover', showSubSection);
+$('.category-subsection').on('mouseout', hideSubSection);
